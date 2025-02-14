7881e8b76fda0b78c55d7e2c6da83e9e52b29ebc
cmaria08 <cmaria@plazalama.com>
2025-02-14 01:07:06 -0400

    Add authentication requirement to permissions route

--- src/routes/app/permissions.tsx
+++ src/routes/app/permissions.tsx
@@ -2,9 +2,11 @@ import { createRoute } from '@tanstack/react-router';
 import { appRoute } from './app';
 import { ROUTES } from '@/constants/routes';
 import { PermissionsPage } from '@/components/app/PermissionsPage';
+import { requireAuth } from '@/lib/route-auth';
 
 export const permissionsRoute = createRoute({
     getParentRoute: () => appRoute,
     path: ROUTES.PERMISSIONS,
+    beforeLoad: () => requireAuth(ROUTES.PERMISSIONS),
     component: PermissionsPage,
 }); 
