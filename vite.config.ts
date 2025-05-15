19f73c681a907fd31ea7afc5851b3b42e76a9e59
CamiloMaria <camilocraft08@gmail.com>
2025-05-15 14:47:04 -0400

    Update base path to root in routes and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       'process.env': env
     },
     plugins: [react()],
-    base: '/dashboard/v2',
+    base: '/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
